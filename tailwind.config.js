85341460df57859f2f3c5c44b1f0a2c0d5ebc001
Your Name <you@example.com>
2024-01-15 22:01:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+theme: {
+    extend: {
+      colors: {
+        amazonclone: {
+          background: "#EAEDED",
+          light_blue: "#232F3A",
+          yellow: "#FEBD69",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
